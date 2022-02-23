844c4c9fbe289886c515c64b48d415681f4c0062
FIRST_NAME LAST_NAME <khadijarejjaoui41@gmail.com>
2022-02-23 21:36:28 +0600

    first commit

---
+++ code.cpp
@@ -0,0 +1,14 @@
+// Solution of 1016 - Distance in beecrowd
+#include <iostream>
+
+
+using namespace std;
+
+int main ()
+{
+ int d;
+ cin >> d;
+ cout << d*2 << " minutos" << endl;
+    system("pause");
+    return 0;
+}
